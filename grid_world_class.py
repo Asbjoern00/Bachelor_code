3fb80e00ab29c30d0cf814da8a20cb7fa75e6601
Asbjoern00 <asbjoern00@gmail.com>
2023-04-13 11:49:36 +0200

    update grid world class

--- grid_world_class.py
+++ grid_world_class.py
@@ -1,78 +1,123 @@
 # Packages:
 import numpy as np
 
+##########################################################################################
+# Define options to be a function of grid, coordinates and steps
+# Takes the agent a length of m steps in one direction.
+def options(map,x,y,step):
+	up = map[x-step, y]
+	right = map[x, y+step]
+	down = map[x+step, y]
+	left = map[x, y-step]
+	return up,right,down,left
+
+# To do: Give a parameter T_max in order to get options in.  
 # Actions: '0' = 'go up', '1' = 'go right', '2'  = 'go down' thus '3' = 'go left'
 # State 0 is upper left, state nS - 1 is lower right.
 # The agent is teleported back to anay other state except for the target state 'nS - 1'
 class grid_world():
 
-	def __init__(self,nS,prim):
+	def __init__(self,nS,T_max):
+		# Construction of the matrix :
 		self.nS = nS
 		d_sqrt = int(nS**(1/2)) # square root of number of states 
 		nS = self.nS
-		self.nA = 4 # there are 4 actions
-
+		self.nA = 4 # 4 options exists. 
 		self.map = np.full((d_sqrt+2,d_sqrt+2),-1) # plus to as we add on both sides
 		center = np.array(range(0,self.nS)).reshape(d_sqrt,d_sqrt)
 		self.map[1:d_sqrt+1,1:d_sqrt+1] = center
 		map = self.map
+		# Grid is now created.
 
-		# We build the transitions matrix P using the map.
+		# We build the transitions matrix P (note that this shouls hold for options).
 		self.P = np.zeros((nS, 4, nS))
+		self.tau = np.zeros((nS, 4, nS))
+
 		for s in range(nS):
-			temp = np.where(s == map)
+			# We need a notion of states between e.g. up and current state.
+			# Find number of time steps
+			row_index=int(np.where(map==s)[0])
+			col_index=int(np.where(map==s)[1])
+			# states under option.
+			down = map[row_index+1:-1,col_index]
+			right = map[row_index,col_index+1:-1]
+			up = map[1:row_index,col_index]
+			left = map[row_index,1:col_index]
+			# length to wall (subtract one to get actual number of states).
+			m_down = len(map[row_index+1:,col_index])
+			m_right = len(map[row_index,col_index+1:])
+			m_up = len(map[:row_index,col_index])
+			m_left = len(map[row_index,:col_index])
 
-			x, y = temp[0][0], temp[1][0]
-			up = map[x-1, y]
-			right = map[x, y+1]
-			down = map[x+1, y]
-			left = map[x, y-1]
 
-			# Action 0: go up.
-			a = 0
-			# Up
-			if up == -1:
-				self.P[s, a, s] += 1.0
-			else:
-				self.P[s, a, up] += 1.0
+			# option setting.
+			# option 0: up.
+			o = 0
+			if m_up>T_max: # further away than maximum.
+				self.P[s, o, up[:T_max]] = 1/(T_max) # use T_max in this case - ommit -1 - T_max elements.
+				self.tau[s,o,up[:T_max]] = range(1,T_max+1) # tau
+			if m_up<=T_max and m_up>1: # closer than max number of steps.
+				self.P[s, o, up] = 1/(m_up-1) # as there is one state less avaivable - also ommit -1.  
+				self.tau[s,o,up] = range(1,m_up) # tau
+
+			if m_up == 1: # length one to wall (i.e. -1)
+				self.P[s, o, s] = 1.0 # Still certain transition.
+				self.tau[s,0,s] = 1
 					
-			# Action 1: go right.
-			a = 1
-			# Right
-			if right == -1:
-				self.P[s, a, s] += 1.0
-			else:
-				self.P[s, a, right] += 1.0
-						
-			# Action 2: go down.
-			a = 2
-			# Down
-			if down == -1:
-				self.P[s, a, s] += 1.0
-			else:
-				self.P[s, a, down] += 1.0
+			# option 1: right.
+			o = 1
+			if m_right>T_max: # further away than maximum.
+				self.P[s, o, right[:T_max]] = 1/(T_max) # use T_max in this case - ommit -1 - T_max elements.
+				self.tau[s,o,right[:T_max]] = range(1,T_max+1) # tau
+			if m_right<=T_max and m_right>1: # closer than max number of steps.
+				self.P[s, o, right] = 1/(m_right-1) # as there is one state less avaivable - also ommit -1.  
+				self.tau[s,o,right] = range(1,m_right) # tau
 
-			# Action 3: go left.
-			a = 3
-			# Left
-			if left == -1:
-				self.P[s, a, s] += 1.0
-			else:
-				self.P[s, a, left] += 1.0							
-				
-							
-			# Set to teleport uniformly
-			if s == self.nS - 1:
-				for a in range(4):
-					for ss in range(self.nS):
-						self.P[s, a, ss] = 1/(self.nS-1) # uniformly for all except of yellow state i.e. 1/(S-1)=1/19 
-						if ss == self.nS-1: #if we get to the same 'yellow' state - happen with zero prob.:
-							self.P[s, a, ss] = 0
+			if m_right == 1: # length one to wall (i.e. -1)
+				self.P[s, o, s] = 1.0 # Still certain transition.
+				self.tau[s,o,s] = 1
+
+			# option 2: down.		
+			o = 2
+			if m_down>T_max: # further away than maximum.
+				self.P[s, o, down[:T_max]] = 1/(T_max) # use T_max in this case - ommit -1 - T_max elements.
+				self.tau[s,o,down[:T_max]] = range(1,T_max+1) # tau
+
+			if m_down<=T_max and m_down>1: # closer than max number of steps.
+				self.P[s, o, down] = 1/(m_down-1) # as there is one state less avaivable - also ommit -1.  
+				self.tau[s,o,down] = range(1,m_down) # tau
+
+			if m_down == 1: # length one to wall (i.e. -1)
+				self.P[s, o, s] = 1.0 # Still certain transition.
+				self.tau[s,o,s] = 1
+
+			# option 3: left.	
+			o = 3
+			if m_left>T_max: # further away than maximum.
+				self.P[s, o, left[:T_max]] = 1/(T_max) # use T_max in this case - ommit -1 - T_max elements.
+				self.tau[s,o,left[:T_max]] = range(1,T_max+1) # tau
+
+			if m_left<=T_max and m_left>1: # closer than max number of steps.
+				self.P[s, o, left] = 1/(m_left-1) # as there is one state less avaivable - also ommit -1.  
+				self.tau[s,o,left] = range(1,m_left) # tau
 			
-		# We build the reward matrix R.
+			if m_left == 1: # length one to wall (i.e. -1)
+				self.P[s, o, s] = 1.0 # Still certain transition.					
+				self.tau[s,o,s] = 1
+				
+
+			# Set to teleport uniformly in the actions setting
+			if s == self.nS - 1:
+				for o in range(4):
+					for ss in range(self.nS):
+						self.P[s, o, ss] = 1/(self.nS-1) # uniformly for all except of yellow state i.e. 1/(S-1)=1/19 
+						if ss == self.nS-1: #if we get to the same 'yellow' state - happen with zero prob.:
+							self.P[s, o, ss] = 0
+			
+		# We build the reward matrix R. This is supposed to only give a reward in the nS-1 state.
 		self.R = np.zeros((nS, 4))
-		for a in range(4):
-			self.R[nS - 1, a] = 1
+		for o in range(4):
+			self.R[nS - 1, o] = 1
 
 		# We (arbitrarily) set the initial state in the top-left corner.
 		self.s = 0
@@ -83,10 +128,44 @@ class grid_world():
 		return self.s
 
 	# Perform a step in the environment for a given action. Return a couple state, reward (s_t, r_t).
-	def step(self, action,prim):
-		if prim==True: # Introduce the actions setting.
-			new_s = np.random.choice(np.arange(self.nS), p=self.P[self.s, action])
-			reward = self.R[self.s, action]
-			self.s = new_s
+	def step(self, option):
+		new_s = np.random.choice(np.arange(self.nS), p=self.P[self.s, option])
+		tau = self.tau[self.s,option,new_s]
+		reward = self.R[self.s, option]
 
-		return new_s, reward
+		self.s = new_s
+		return new_s, reward, tau
+	
+
+def VI(env, max_iter = 10**3, epsilon = 10**(-2)):
+
+	# The variable containing the optimal policy estimate at the current iteration.
+	policy = np.zeros(env.nS, dtype=int)
+	niter = 0
+
+	# Initialise the value and epsilon as proposed in the course.
+	V0 = np.zeros(env.nS)
+	V1 = np.zeros(env.nS)
+
+	# The main loop of the Value Iteration algorithm.
+	while True:
+		niter += 1
+		for s in range(env.nS):
+			for a in range(env.nA):
+				temp = env.R[s, a] + sum([V * p for (V, p) in zip(V0, env.P[s, a])])
+				if (a == 0) or (temp > V1[s]):
+					V1[s] = temp
+					policy[s] = a
+		
+		# Testing the stopping criterion (+1 abitrary stop when 'max_iter' is reached).
+		gain = 0.5*(max(V1 - V0) + min(V1 - V0))
+		diff  = [abs(x - y) for (x, y) in zip(V1, V0)]
+		if (max(diff) - min(diff)) < epsilon:
+			return niter, V0, policy, gain
+		else:
+			V0 = V1
+			V1 = np.zeros(env.nS)
+		if niter > max_iter:
+			print("No convergence in VI after: ", max_iter, " steps!")
+			return niter, V0, policy, gain
+
