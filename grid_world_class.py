e69e9305729bed50a00fccd244000ad61b5aeeff
Asbjoern00 <asbjoern00@gmail.com>
2023-04-14 09:33:04 +0200

    implementation of P_eq

--- grid_world_class.py
+++ grid_world_class.py
@@ -32,6 +32,8 @@ class grid_world():
 		# We build the transitions matrix P (note that this shouls hold for options).
 		self.P = np.zeros((nS, 4, nS))
 		self.tau = np.zeros((nS, 4, nS))
+		self.tau_bar = np.zeros((nS,4)) # expected holding time.
+		self.P_eq = np.zeros((nS,4,nS))
 
 		for s in range(nS):
 			# We need a notion of states between e.g. up and current state.
@@ -41,13 +43,14 @@ class grid_world():
 			# states under option.
 			down = map[row_index+1:-1,col_index]
 			right = map[row_index,col_index+1:-1]
-			up = np.flip(map[1:row_index,col_index])
-			left = np.flip(map[row_index,1:col_index])
+			up = np.flip(map[1:row_index,col_index]) # necessary to flip this (later index)
+			left = np.flip(map[row_index,1:col_index]) # necessary to flip this
 			# length to wall (subtract one to get actual number of states).
 			m_down = len(map[row_index+1:,col_index])
 			m_right = len(map[row_index,col_index+1:])
 			m_up = len(map[:row_index,col_index])
 			m_left = len(map[row_index,:col_index])
+
 
 
 			# option setting.
@@ -59,10 +62,15 @@ class grid_world():
 			if m_up<=T_max and m_up>1: # closer than max number of steps.
 				self.P[s, o, up] = 1/(m_up-1) # as there is one state less avaivable - also ommit -1.  
 				self.tau[s,o,up] = range(1,m_up) # tau
-
+			# find expected holding time for state option pair (see appendix F).
+			self.tau_bar[s,o]=(T_max + 1)/2
+			# define equivalent probabilities
+			self.P_eq[s,o] = (1-0.1)/(self.tau_bar[s,o])*self.P[s,o] 
 			if m_up == 1: # length one to wall (i.e. -1)
 				self.P[s, o, s] = 1.0 # Still certain transition.
 				self.tau[s,o,s] = 1.0
+			self.P_eq[s,o,s] = (1-0.1)/(self.tau_bar[s,o])*(self.P[s,o,s]-1)+1
+ 
 					
 			# option 1: right.
 			o = 1
@@ -72,10 +80,15 @@ class grid_world():
 			if m_right<=T_max and m_right>1: # closer than max number of steps.
 				self.P[s, o, right] = 1/(m_right-1) # as there is one state less avaivable - also ommit -1.  
 				self.tau[s,o,right] = range(1,m_right) # tau
-
+			# find expected holding time for state option pair (see appendix F).
+			self.tau_bar[s,o]=(T_max + 1)/2
+			# define equivalent probabilities
+			self.P_eq[s,o] = (1-0.1)/(self.tau_bar[s,o])*self.P[s,o] 
 			if m_right == 1: # length one to wall (i.e. -1)
 				self.P[s, o, s] = 1.0 # Still certain transition.
 				self.tau[s,o,s] = 1.0
+			self.P_eq[s,o,s] = (1-0.1)/(self.tau_bar[s,o])*(self.P[s,o,s]-1)+1
+
 
 			# option 2: down.		
 			o = 2
@@ -86,10 +99,15 @@ class grid_world():
 			if m_down<=T_max and m_down>1: # closer than max number of steps.
 				self.P[s, o, down] = 1/(m_down-1) # as there is one state less avaivable - also ommit -1.  
 				self.tau[s,o,down] = range(1,m_down) # tau
-
+			# find expected holding time for state option pair (see appendix F).
+			self.tau_bar[s,o]=(T_max + 1)/2
+			# define equivalent probabilities
+			self.P_eq[s,o] = (1-0.1)/(self.tau_bar[s,o])*self.P[s,o] 
 			if m_down == 1: # length one to wall (i.e. -1)
 				self.P[s, o, s] = 1.0 # Still certain transition.
 				self.tau[s,o,s] = 1.0
+			self.P_eq[s,o,s] = (1-0.1)/(self.tau_bar[s,o])*(self.P[s,o,s]-1)+1
+
 
 			# option 3: left.	
 			o = 3
@@ -100,10 +118,15 @@ class grid_world():
 			if m_left<=T_max and m_left>1: # closer than max number of steps.
 				self.P[s, o, left] = 1/(m_left-1) # as there is one state less avaivable - also ommit -1.  
 				self.tau[s,o,left] = range(1,m_left) # tau
-			
+			# find expected holding time for state option pair (see appendix F).
+			self.tau_bar[s,o]=(T_max + 1)/2
+			# define equivalent probabilities
+			self.P_eq[s,o] = (1-0.1)/(self.tau_bar[s,o])*self.P[s,o] 			
 			if m_left == 1: # length one to wall (i.e. -1)
 				self.P[s, o, s] = 1.0 # Still certain transition.					
 				self.tau[s,o,s] = 1.0
+			self.P_eq[s,o,s] = (1-0.1)/(self.tau_bar[s,o])*(self.P[s,o,s]-1)+1
+
 				
 
 			# Set to teleport uniformly in the actions setting
@@ -111,9 +134,14 @@ class grid_world():
 				for o in range(4):
 					for ss in range(self.nS):
 						self.P[s, o, ss] = 1/(self.nS-1) # uniformly for all except of yellow state i.e. 1/(S-1)=1/19 
-						self.tau[s,o,ss] = 1.0
+						self.tau[s, o, ss] = 1.0 # always one unit of time here.
+						self.tau_bar[s,o] = 1 # always one unit (teleportation)
+						self.P_eq[s,o,ss] = (1-0.1)/(self.tau_bar[s,o])*(self.P[s,o,ss])
+
 						if ss == self.nS-1: #if we get to the same 'yellow' state - happen with zero prob.:
 							self.P[s, o, ss] = 0
+							self.P_eq[s,o,ss] = (1-0.1)/(self.tau_bar[s,o])*(self.P[s,o,ss]-1)+1
+
 			
 		# We build the reward matrix R. This is supposed to only give a reward in the nS-1 state.
 		self.R = np.zeros((nS, 4))
@@ -133,6 +161,41 @@ class grid_world():
 		new_s = np.random.choice(np.arange(self.nS), p=self.P[self.s, option])
 		tau = self.tau[self.s,option,new_s]
 		reward = self.R[self.s, option]
-
 		self.s = new_s
 		return new_s, reward, tau
+
+
+
+# Value iteration also works for equivalent MDP - this is how optimal gain is found for options.
+
+def VI(env, max_iter = 10**3, epsilon = 10**(-4)):
+
+	# The variable containing the optimal policy estimate at the current iteration.
+	policy = np.zeros(env.nS, dtype=int)
+	niter = 0
+
+	# Initialise the value and epsilon as proposed in the course.
+	V0 = np.zeros(env.nS)
+	V1 = np.zeros(env.nS)
+
+	# The main loop of the Value Iteration algorithm.
+	while True:
+		niter += 1
+		for s in range(env.nS):
+			for a in range(env.nA):
+				temp = env.R[s, a] + sum([V * p for (V, p) in zip(V0, env.P_eq[s, a])])
+				if (a == 0) or (temp > V1[s]):
+					V1[s] = temp
+					policy[s] = a
+		
+		# Testing the stopping criterion (+1 abitrary stop when 'max_iter' is reached).
+		gain = 0.5*(max(V1 - V0) + min(V1 - V0))
+		diff  = [abs(x - y) for (x, y) in zip(V1, V0)]
+		if (max(diff) - min(diff)) < epsilon:
+			return niter, V0, policy, gain
+		else:
+			V0 = V1
+			V1 = np.zeros(env.nS)
+		if niter > max_iter:
+			print("No convergence in VI after: ", max_iter, " steps!")
+			return niter, V0, policy, gain
