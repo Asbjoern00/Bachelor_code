45ab3dd0f526c164c35181fc60c38cad50c64e2a
Asbjoern00 <asbjoern00@gmail.com>
2023-04-23 15:18:58 +0200

    initial experiments

--- riverswim_class.py
+++ riverswim_class.py
@@ -28,6 +28,8 @@ class riverswim():
         self.T_max = T_max
         
         self.tau = np.full((nS, 2, self.T_max), 1/self.T_max) # Holding times. For now assumed uniform
+        self.tau[:,0,:] = 0
+        self.tau[:,0,0] = 1 
  
         
         self.beta = np.full(nS,1/nS) # uniform termination prob. 
@@ -86,67 +88,10 @@ class riverswim():
 # Perform a step in the environment for a given action. Return a couple state, reward (s_t, r_t).
 # Idea is to make an if else statement like before.
     def step(self, action):
-        if action==0: #always holding time of 1 for left.
-            tau=1
-        else:
-            tau = np.random.choice(range(1,self.T_max+1),replace = True)  # draw holding time uniformly i.e. term prob.
+        tau = np.random.choice(range(1,self.T_max+1), 1, p = self.tau[self.s,action])[0]  # draw holding time uniformly
         reward = 0
         for i in range(1,tau+1):
-            new_s = np.random.choice(np.arange(self.nS), p=self.P_eq[self.s, action])
+            new_s = np.random.choice(np.arange(self.nS), p=self.P[self.s, action])
             reward += self.R[self.s, action] # get termination reward (will be last)
-            self.s = new_s # get termination reward (will be last)
+            self.s = new_s # set state
         return new_s, reward, tau
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-'''
-env = riverswim(nS = 10, T_max = 9)
-
-def QL_SMDP(env,T):
-    policy = np.zeros(env.nS)
-    niter = 1
-    epsilon = 1/np.sqrt(niter) # exploration term
-    Nk = np.zeros((env.nS, env.nA), dtype=int) # Number of occurences of (s, a) at the end of the last episode.
-    Nsas = np.zeros((env.nS, env.nA, env.nS), dtype=int) # Number of occureces of (s, a, s').
-	# Initialise the value and epsilon as proposed in the course.
-    Q0 = np.zeros((env.nS,env.nA))
-    s = env.s # initial state.
-    a = np.argmax(Q0[env.s,:])
-    for i in range(T):
-            print(niter)
-            niter +=1 # increment t.
-            new_s, reward, tau = env.step(a) # take new action
-            Nk[s,a] += 1
-            Nsas[s,a,new_s] += 1
-            alpha = 2/((Nk[s,a])**(2/3)+1)
-            delta = reward + np.max(Q0[new_s,:])-Q0[s,a]
-            Q0[s,a] = Q0[s,a] + alpha*delta
-            s = new_s # update
-            dum = np.random.choice(2,replace=True,p = [epsilon,1-epsilon])
-            if dum ==1: # i.e. greedily chosen:
-                a = np.argmax(Q0[env.s,:]) # find next action
-            else:
-                a = np.random.choice(env.nA,replace = True)
-            policy = np.argmax(Q0,axis = 1)
-    return policy,Q0
-
-print(QL_SMDP(env,T=10**5))
-'''
